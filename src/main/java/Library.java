d184a040a10b4df8993450b00a048b8ab1041f88
lushpurr <lushpurr@gmail.com>
2021-01-06 19:54:45 +0000

    MVP completed

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 
 public class Library {
     private ArrayList<Book> books;
+    private int capacity;
 
-    public Library(){
+    public Library(int capacity){
         this.books = new ArrayList<Book>();
+        this.capacity = capacity;
     }
 
     public int bookCount() {
@@ -14,4 +16,12 @@ public class Library {
     public void addBookToLibrary(Book book) {
         this.books.add(book);
     }
+
+    public void canLibraryHoldStock(Book book) {
+        if (this.books.size()<this.capacity){
+            this.addBookToLibrary(book);
+        }
+    }
+
+
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -15,7 +15,7 @@ public class LibraryTest {
 
     @Before
     public void before(){
-        library = new Library();
+        library = new Library(5);
         book = new Book("Don Quixote", "Miguel De Cervantes", "Adventure");
         book1 = new Book("Gullivers Travels","Jonathan Swift","Adventure");
         book2 = new Book("Robinson Crusoe","Daniel Dafoe", "Adventure");
@@ -37,5 +37,18 @@ public class LibraryTest {
         library.addBookToLibrary(book1);
         assertEquals(2, library.bookCount());
     }
+
+
+    @Test
+    public void ifLibraryHasRoomAddStock(){
+        library.canLibraryHoldStock(book);
+        library.canLibraryHoldStock(book1);
+        library.canLibraryHoldStock(book2);
+        library.canLibraryHoldStock(book3);
+        library.canLibraryHoldStock(book4);
+        library.canLibraryHoldStock(book5);
+        assertEquals(5, library.bookCount());
+    }
+
 }
 

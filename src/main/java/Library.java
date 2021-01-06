bcf2bb963d3c579573727918e121b696b81c518d
lushpurr <lushpurr@gmail.com>
2021-01-06 16:56:17 +0000

    initial commit and setup

---
+++ src/main/java/Library.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<Book> books;
+
+    public Library(){
+        this.books = new ArrayList<Book>();
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book();
+
+    }
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.bookCount());
+    }
+}
+
